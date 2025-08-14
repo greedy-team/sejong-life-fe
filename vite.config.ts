bc32cc17239d8642cd0e3410cd055180b474a6b1
ChangwooJ <wjdckddn99@naver.com>
2025-08-14 13:31:01 +0900

    chore: 테스트 도구 세팅

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
+import { defineConfig as defineVitestConfig } from 'vitest/config';
+
+export default defineVitestConfig({
   plugins: [react()],
-})
+  test: {
+    environment: 'jsdom',
+    globals: true,
+    setupFiles: './src/setupTests.ts',
+  },
+});
+
