5334c609921dd84781ac9f320164b3a7263fb7a3
Shin Jihoon <tobeabestns@naver.com>
2025-08-29 16:33:19 +0900

    refactor: textType 관련 util 코드 분리

--- src/components/place-item-container/model/type.ts
+++ src/components/place-item-container/model/type.ts
@@ -1,28 +1,7 @@
 import type { PlaceItemCardProps } from '../../place-item-card/model/type';
-import { type ElementType } from 'react';
 
 export interface ItemContainerProps {
   title: string;
   items: PlaceItemCardProps[];
-}
-
-export interface TextTypeProps {
-  className?: string;
-  showCursor?: boolean;
-  hideCursorWhileTyping?: boolean;
-  cursorCharacter?: string | React.ReactNode;
-  cursorBlinkDuration?: number;
-  cursorClassName?: string;
-  text: string | string[];
-  as?: ElementType;
-  typingSpeed?: number;
-  initialDelay?: number;
-  pauseDuration?: number;
-  deletingSpeed?: number;
-  loop?: boolean;
-  textColors?: string[];
-  variableSpeed?: { min: number; max: number };
-  onSentenceComplete?: (sentence: string, index: number) => void;
-  startOnVisible?: boolean;
-  reverseMode?: boolean;
+  iconSrc?: string;
 }
