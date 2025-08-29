daea2713f78f8cf9e449bfb81f9b195a33951847
ChangwooJ <wjdckddn99@naver.com>
2025-08-29 19:16:07 +0900

    refactor: 메뉴 연결

--- src/components/share/PageRouterButton.tsx
+++ src/components/share/PageRouterButton.tsx
@@ -1,18 +1,34 @@
-import type { ReactNode } from 'react';
+import { type ReactNode } from 'react';
 import { useNavigate } from 'react-router-dom';
+import type { CategoryProps } from '../../types/type';
+import { useCategory } from '../../hooks/useCategory';
 
 interface PageRouterButtonProps {
+  category?: CategoryProps;
   children: ReactNode;
   to: string;
   icon?: string;
 }
 
-const PageRouterButton = ({ children, to, icon }: PageRouterButtonProps) => {
+const PageRouterButton = ({
+  category,
+  children,
+  to,
+  icon,
+}: PageRouterButtonProps) => {
   const navigate = useNavigate();
+  const { setSelectedCategory } = useCategory();
+
+  const handleMenuClick = (to: string) => {
+    if (category) {
+      setSelectedCategory(category);
+    }
+    navigate(to);
+  };
 
   return (
     <button
-      onClick={() => navigate(to)}
+      onClick={() => handleMenuClick(to)}
       className="relative h-24 w-24 cursor-pointer rounded-2xl border-none bg-gray-100 p-2 transition-all duration-200 ease-in-out hover:scale-105 hover:bg-gray-200"
     >
       {icon && (
