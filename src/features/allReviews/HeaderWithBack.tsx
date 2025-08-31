bad0869fb47faa3af06242047a6eb26bd5c07f76
zldn109 <ss771009@naver.com>
2025-08-31 15:31:34 +0900

    feat: 뒤로가기 버튼 연결

--- src/features/allReviews/HeaderWithBack.tsx
+++ src/features/allReviews/HeaderWithBack.tsx
@@ -1,10 +1,16 @@
+import { useNavigate } from 'react-router-dom';
+
 const HeaderWithBack = () => {
+  const navigate = useNavigate();
   return (
     <>
-      <header className="flex h-14 w-full border-b border-b-[#EEEFF1] px-20 py-2">
-        <button className="flex items-center gap-5">
+      <header className="flex h-14 w-full border-b border-b-[#EEEFF1] px-15 py-2">
+        <button
+          onClick={() => navigate(-1)}
+          className="flex items-center gap-5"
+        >
           <img src="/asset/all-review/backArrow.svg" alt="뒤로가기"></img>
-          그리디카페
+          리뷰
         </button>
       </header>
     </>
