55a89b59e77f97ebdb17dd40e954ee9f14b5b32f
zldn109 <ss771009@naver.com>
2025-09-09 18:34:56 +0900

    fix: 버셀 오류 수정

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { AppProviders } from './context/Provider.tsx';
-import './index.css';
 import App from './App.tsx';
+import './index.css';
 
 // if (import.meta.env.DEV) {
 //   const { worker } = await import('./mock/browser');
