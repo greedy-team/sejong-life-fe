0c21429151ef6e9a5b0ac4c46db83a3464865111
zldn109 <ss771009@naver.com>
2025-09-15 00:24:27 +0900

    refactor: 포토그리드 너비 최대로 수정

--- src/features/placeDetail/PhotoGrid.tsx
+++ src/features/placeDetail/PhotoGrid.tsx
@@ -5,7 +5,7 @@ interface PhotoGridProps {
 
 const PhotoGrid = ({ images, onImageClick }: PhotoGridProps) => {
   return (
-    <div className="grid max-h-[40rem] max-w-[50rem] grid-cols-3 gap-0.5 overflow-y-auto bg-white p-0.5">
+    <div className="grid max-h-[40rem] grid-cols-3 gap-0.5 overflow-y-auto bg-white p-0.5">
       {images.map((src, i) => (
         <img
           className="aspect-square w-full cursor-pointer object-cover"
