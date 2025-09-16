b39e5db304bc55a294fd56e50b04bf0df5fab5f2
zldn109 <ss771009@naver.com>
2025-09-16 20:53:35 +0900

    style: 카테고리에 이모지 추가

--- src/features/explore/components/CategoryFilter.tsx
+++ src/features/explore/components/CategoryFilter.tsx
@@ -29,16 +29,33 @@ const CategoryFilter = () => {
     setSearchParams({ category: category.categoryName });
   };
 
+  const getEmoji = (name: string) => {
+    switch (name) {
+      case '전체':
+        return <img src="/asset/pageRouterButton/allItemIcon.svg" alt="전체" />;
+      case '식당':
+        return (
+          <img src="/asset/pageRouterButton/restaurantIcon.svg" alt="식당" />
+        );
+      case '카페':
+        return <img src="/asset/pageRouterButton/cafeIcon.svg" alt="카페" />;
+      default:
+        return '';
+    }
+  };
   return (
     <div>
       <div className="mx-auto flex w-[70%] justify-center gap-3 sm:w-[80%] sm:gap-10">
         {categories.map((category) => (
           <button
             key={category.categoryId}
-            className={`relative z-10 flex h-[46px] w-[130px] cursor-pointer items-center justify-center rounded-xl font-semibold transition-colors duration-100 hover:scale-105 ${isSelected(category) ? 'bg-gray-200' : 'bg-gray-100'} `}
+            className={`relative z-10 flex h-[46px] w-[120px] cursor-pointer items-center justify-center rounded-xl font-semibold transition-colors duration-100 hover:scale-105 ${isSelected(category) ? 'bg-gray-200' : 'bg-gray-100'} `}
             onClick={() => handleCategoryClick(category)}
           >
-            {category.categoryName}
+            <div className="flex items-center gap-1 sm:gap-3">
+              {getEmoji(category.categoryName)}
+              <span>{category.categoryName}</span>
+            </div>
           </button>
         ))}
       </div>
