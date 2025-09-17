5cc44560fa494a5a51a6fa514fabb12137df78a3
zldn109 <ss771009@naver.com>
2025-09-17 17:43:52 +0900

    fix: 조회수 쿠키 설정

--- src/api/api.ts
+++ src/api/api.ts
@@ -5,6 +5,7 @@ const BASE_URL = import.meta.env.VITE_API_URL;
 export const api = axios.create({
   baseURL: BASE_URL,
   timeout: 5000,
+  withCredentials: true,
 });
 
 export const authApi = axios.create({
