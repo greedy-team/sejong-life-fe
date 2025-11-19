80e91a4db8f921fec210c04491dde411bd3de45f
changwooj <wjdckddn99@naver.com>
2025-11-19 14:27:55 +0900

    refactor: 모바일 뷰 반응형 수정

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,11 +1,17 @@
 import { Outlet } from 'react-router-dom';
 import Header from './components/Header';
+import Footer from './components/Footer';
 
 const Layout = () => {
   return (
-    <div className="min-h-screen">
+    <div className="flex min-h-screen flex-col">
       <Header />
-      <Outlet />
+      <main className="w-full flex-1">
+        <div className="w-full max-w-[1920px]">
+          <Outlet />
+        </div>
+      </main>
+      <Footer />
     </div>
   );
 };
