1a2793633053a040e32ae429ba53f54b060af1b4
zldn109 <ss771009@naver.com>
2026-01-13 00:20:03 +0900

    feat: 검색 기능 api 연결

---
+++ src/api/searchResultApi.ts
@@ -0,0 +1,28 @@
+import { api } from './api';
+
+type SearchParams = {
+  keyword: string;
+  category?: string;
+};
+
+export const fetchSearchResult = async ({
+  keyword,
+  category = '전체',
+}: SearchParams) => {
+  const trimmed = keyword.trim();
+  if (!trimmed) return [];
+
+  try {
+    const response = await api.get('/api/places', {
+      params: {
+        category,
+        keyword: trimmed,
+      },
+    });
+
+    return response.data;
+  } catch (error) {
+    console.error('검색 실패', error);
+    return [];
+  }
+};
--- src/components/share/SearchBar.tsx
+++ src/components/share/SearchBar.tsx
@@ -1,14 +1,34 @@
+import { useState } from 'react';
+import { fetchSearchResult } from '../../api/searchResultApi';
+
 const SearchBar = () => {
+  const [keyword, setKeyword] = useState('');
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const response = await fetchSearchResult({ keyword });
+    console.log(response);
+  };
+
   return (
-    <div className="mt-10 flex w-[80%] overflow-hidden rounded-2xl border border-[#8BE34A] lg:w-[60%]">
+    <form
+      onSubmit={handleSubmit}
+      className="mt-10 flex w-[80%] overflow-hidden rounded-2xl border border-[#8BE34A] lg:w-[60%]"
+    >
       <input
+        value={keyword}
+        onChange={(e) => setKeyword(e.target.value)}
         className="text- flex-1 px-5 py-4 outline-none"
         placeholder="궁금한 장소를 검색해보세요 !"
       />
-      <button className="flex items-center justify-center bg-[#8BE34A] px-7">
+      <button
+        type="submit"
+        className="flex items-center justify-center bg-[#8BE34A] px-7"
+      >
         <img src="/asset/searchbar/search.svg" alt="search"></img>
       </button>
-    </div>
+    </form>
   );
 };
 
