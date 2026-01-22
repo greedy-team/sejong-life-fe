ff50c9eef1cc77f320b3da1964301421e5efcde5
zldn109 <ss771009@naver.com>
2026-01-23 02:55:23 +0900

    style: 북마크 svg 변경

--- src/components/myPage/LikePlaces.tsx
+++ src/components/myPage/LikePlaces.tsx
@@ -2,7 +2,7 @@ const LikePlaces = () => {
   return (
     <button className="flex h-38 cursor-pointer flex-col items-center justify-center gap-3 rounded-3xl bg-white drop-shadow-[0_6px_12px_rgba(139,227,74,0.4)] transition-shadow duration-200 hover:shadow-[0_8px_18px_rgba(139,227,74,0.35)] lg:h-60">
       <img
-        src="/asset/mypage/my-review.svg"
+        src="/asset/mypage/bookmark.svg"
         alt="저장한 장소"
         className="h-10 w-10 lg:h-20 lg:w-20"
       />
