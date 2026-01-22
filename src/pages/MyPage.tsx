3d2c08338de4ba3df357c7995707b343f5f83dbc
zldn109 <ss771009@naver.com>
2026-01-23 02:19:25 +0900

    feat: 로그아웃, 탈퇴 버튼 UI 구현

--- src/pages/MyPage.tsx
+++ src/pages/MyPage.tsx
@@ -4,12 +4,17 @@ import LikePlaces from '../components/myPage/LikePlaces';
 
 function MyPage() {
   return (
-    <div className="mx-auto mt-10 flex w-[85%] flex-col gap-5 lg:w-[60%] lg:gap-8">
+    <div className="mx-auto mt-10 flex w-[85%] flex-col gap-8 lg:w-[60%] lg:gap-8">
       <ProfileCard />
 
       <div className="flex grid grid-cols-2 gap-5 lg:gap-8">
         <MyReviews />
         <LikePlaces />
+      </div>
+
+      <div className="mt-15 flex flex-col gap-3 text-xs">
+        <button className="lg:text-xl">로그아웃하기</button>
+        <button className="text-gray-400 lg:text-xl">탈퇴하기</button>
       </div>
     </div>
   );
