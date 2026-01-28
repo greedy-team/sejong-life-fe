42aa9f4e16549e7684dcae88cd903f5269292677
zldn109 <ss771009@naver.com>
2026-01-29 06:37:41 +0900

    feat: 탈퇴기능 추가

--- src/pages/MyPage.tsx
+++ src/pages/MyPage.tsx
@@ -2,10 +2,15 @@ import ProfileCard from '../components/myPage/ProfileCard';
 import MyReviews from '../components/myPage/MyReviews';
 import LikePlaces from '../components/myPage/LikePlaces';
 import { useAuth } from '../hooks/useAuth';
-import { Navigate } from 'react-router-dom';
+import { Navigate, useNavigate } from 'react-router-dom';
+import deleteUser from '../features/myPage/apis/deleteUser';
+import { toast } from 'react-toastify';
+import axios from 'axios';
 
 function MyPage() {
   const { isLoggedIn, setIsLoggedIn } = useAuth();
+  const navigate = useNavigate();
+
   if (!isLoggedIn) {
     return <Navigate to="/" replace />;
   }
@@ -13,6 +18,28 @@ function MyPage() {
   const handleLogout = () => {
     localStorage.removeItem('accessToken');
     setIsLoggedIn(false);
+  };
+
+  const handleMembershipCancellation = async () => {
+    const ok = confirm('정말 탈퇴하시겠습니까?');
+    if (!ok) return false;
+
+    try {
+      await deleteUser();
+
+      localStorage.removeItem('accessToken');
+      setIsLoggedIn(false);
+
+      toast.success('탈퇴 되었습니다.');
+      navigate(`/`, { replace: true });
+    } catch (err) {
+      if (axios.isAxiosError(err) && err.response) {
+        toast.error(err.response.data.message);
+      } else {
+        console.error(err);
+        toast.error('알 수 없는 오류가 발생했습니다.');
+      }
+    }
   };
 
   return (
@@ -31,7 +58,10 @@ function MyPage() {
         >
           로그아웃하기
         </button>
-        <button className="cursor-pointer text-gray-400 lg:text-xl">
+        <button
+          onClick={() => handleMembershipCancellation()}
+          className="cursor-pointer text-gray-400 lg:text-xl"
+        >
           탈퇴하기
         </button>
       </div>
