991a217b2648f2cee6ce2996ce964ee634a9a301
zldn109 <ss771009@naver.com>
2026-02-02 23:20:10 +0900

    index on 166-feat-mypage-favorite-places: a55a9a8 feat: 뒤로가기 헤더 연동

--- src/pages/MyPlacesPage.tsx
+++ src/pages/MyPlacesPage.tsx
@@ -1,4 +1,21 @@
+import { useEffect, useState } from 'react';
+import { getMyPlaces } from '../features/myPage/apis/getMyPlaces';
+import type { Place } from '../types/type';
+
 function MyPlacesPage() {
+  const [myPlaces, setMyPlaces] = useState<Place[]>([]);
+
+  useEffect(() => {
+    const fetchMyPlaces = async () => {
+      const res = await getMyPlaces();
+      console.log(res);
+
+      setMyPlaces(res);
+    };
+
+    fetchMyPlaces();
+  }, []);
+
   return <div>하이</div>;
 }
 
