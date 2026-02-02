a15e88322afac6db1768e8b8cb86b4baa90878b7
zldn109 <ss771009@naver.com>
2026-02-03 00:26:37 +0900

    feat: 마이페이지 저장된 플레이스에 즐겨찾기한 장소 띄우기

--- src/features/explore/apis/getFavoritePlaces.ts
+++
@@ -1,6 +0,0 @@
-import { authApi } from '../../../api/api';
-
-export const getFavoritePlaces = async () => {
-  const response = await authApi.get(`/api/places/favorites/me`);
-  return response.data;
-};
--- src/pages/MyPlacesPage.tsx
+++ src/pages/MyPlacesPage.tsx
@@ -1,9 +1,12 @@
 import { useEffect, useState } from 'react';
 import { getMyPlaces } from '../features/myPage/apis/getMyPlaces';
 import type { PlaceProps } from '../types/type';
+import PlaceItemCard from '../components/place-item-card/PlaceItemCard';
+import { useFavorites } from '../features/myPage/hooks/useFavorites';
 
 function MyPlacesPage() {
   const [myPlaces, setMyPlaces] = useState<PlaceProps[]>([]);
+  const { isFavorite, handleToggleFavorite } = useFavorites();
 
   useEffect(() => {
     const fetchMyPlaces = async () => {
@@ -16,7 +19,19 @@ function MyPlacesPage() {
     fetchMyPlaces();
   }, []);
 
-  return <div>하이</div>;
+  return (
+    <div className="mx-auto mt-10 mb-20 grid w-[80%] grid-cols-1 gap-3 sm:grid-cols-2 lg:grid-cols-3">
+      {myPlaces.map((place) => (
+        <PlaceItemCard
+          key={place.placeId}
+          placeInfo={place}
+          className="w-full"
+          isFavorite={isFavorite(place.placeId)}
+          onToggleFavorite={handleToggleFavorite}
+        />
+      ))}
+    </div>
+  );
 }
 
 export default MyPlacesPage;
