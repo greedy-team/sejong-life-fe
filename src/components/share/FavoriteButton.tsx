c139de398afa197874c7e25a3a43240b5bd0fd09
zldn109 <ss771009@naver.com>
2026-02-03 20:16:18 +0900

    feat: 장소 저장시 로그인 여부 확인 및 미로그인 시 로그인 모달 띄우기

--- src/components/share/FavoriteButton.tsx
+++ src/components/share/FavoriteButton.tsx
@@ -1,3 +1,9 @@
+import { useAuth } from '../../hooks/useAuth';
+import { useState } from 'react';
+import { toast } from 'react-toastify';
+import LoginModal from '../../features/login/components/LoginModal';
+import LoginWidget from '../../features/login/components/LoginWidget';
+
 interface FavoriteButtonProps {
   className?: string;
   isFavorite: boolean;
@@ -9,23 +15,44 @@ const FavoriteButton = ({
   isFavorite,
   onToggleFavorite,
 }: FavoriteButtonProps) => {
+  const { isLoggedIn } = useAuth();
+  const [isLoginOpen, setIsLoginOpen] = useState(false);
+
+  const handleFavoriteClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    e.stopPropagation();
+
+    if (isLoggedIn) {
+      onToggleFavorite(e);
+    } else {
+      toast.error('좋아요를 남기려면 로그인해주세요');
+      setIsLoginOpen(true);
+    }
+  };
+
   return (
-    <button onClick={onToggleFavorite} className={`${className}`}>
-      {!isFavorite && (
-        <img
-          src="/asset/place-item-card/bookmark.svg"
-          alt="비워진 북마크"
-          className="h-7"
-        />
-      )}
-      {isFavorite && (
-        <img
-          src="/asset/place-item-card/fillBookmark.svg"
-          alt="채워진 북마크"
-          className="h-7"
-        />
-      )}
-    </button>
+    <>
+      <button onClick={handleFavoriteClick} className={`${className}`}>
+        {!isFavorite && (
+          <img
+            src="/asset/place-item-card/bookmark.svg"
+            alt="비워진 북마크"
+            className="h-7"
+          />
+        )}
+        {isFavorite && (
+          <img
+            src="/asset/place-item-card/fillBookmark.svg"
+            alt="채워진 북마크"
+            className="h-7"
+          />
+        )}
+      </button>
+
+      <LoginModal isOpen={isLoginOpen} onClose={() => setIsLoginOpen(false)}>
+        <LoginWidget onClose={() => setIsLoginOpen(false)} />
+      </LoginModal>
+    </>
   );
 };
 
