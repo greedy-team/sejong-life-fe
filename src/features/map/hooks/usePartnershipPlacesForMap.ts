8ad2f85f756827170eb89c6b2b868b5363215930
zldn109 <ss771009@naver.com>
2026-03-03 00:20:33 +0900

    refactor: 쿼리키 lib 폴더로 이동

--- src/features/map/hooks/usePartnershipPlacesForMap.ts
+++ src/features/map/hooks/usePartnershipPlacesForMap.ts
@@ -1,9 +1,10 @@
 import { useQuery } from '@tanstack/react-query';
 import { fetchFilteredPlaces } from '../../explore/apis/placeApi';
+import { queryKeys } from '../../../lib/query/queryKeys';
 
 export const usePartnershipPlacesForMap = () => {
   return useQuery({
-    queryKey: ['places', 'map', 'partnership'],
+    queryKey: queryKeys.places.partnershipMap(),
     queryFn: () => fetchFilteredPlaces('전체', [], true, 0, 1000),
     select: (data) => {
       return data.data?.places ?? [];
