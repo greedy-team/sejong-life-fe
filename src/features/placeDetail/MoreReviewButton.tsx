a0a745eff2b8647fe2281928c8c7fa010138127f
zldn109 <ss771009@naver.com>
2026-03-09 22:25:20 +0900

    refactor: 장소 상세 페이지에 보이는 리뷰 프리뷰 개수를 2개에서 5개로 증가

--- src/features/placeDetail/MoreReviewButton.tsx
+++ src/features/placeDetail/MoreReviewButton.tsx
@@ -14,7 +14,7 @@ const MoreReviewButton = ({ reviewCount }: ReviewStatsProps) => {
         onClick={() => navigate(`/detail/${id}/reviews`)}
         className="mb-10 cursor-pointer rounded-xl border border-[#8BE34A] bg-[#77db30] px-6 py-3 font-semibold text-white hover:bg-[#8BE34A]"
       >
-        더 많은 리뷰 보러가기 (+{reviewCount - 2})
+        더 많은 리뷰 보러가기 (+{reviewCount - 5})
       </a>
     </div>
   );
