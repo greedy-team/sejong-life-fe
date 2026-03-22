5748cc834a53b59ac283b35c410dcb7a6415ada6
zldn109 <ss771009@naver.com>
2026-03-22 15:04:15 +0900

    fix: 서치바의 검색버튼이 모바일에서 잘리는 이슈 수정

--- src/components/share/SearchBar.tsx
+++ src/components/share/SearchBar.tsx
@@ -21,17 +21,17 @@ const SearchBar = () => {
   return (
     <form
       onSubmit={handleSubmit}
-      className="mt-10 flex w-[80%] overflow-hidden rounded-2xl border border-[#8BE34A] lg:w-[60%]"
+      className="mt-10 flex w-[80%] rounded-2xl border border-[#8BE34A] lg:w-[60%]"
     >
       <input
         value={keyword}
         onChange={(e) => setKeyword(e.target.value)}
-        className="flex-1 px-5 py-4 text-sm outline-none lg:text-base"
+        className="flex-1 rounded-l-2xl px-5 py-4 text-sm outline-none lg:text-base"
         placeholder="궁금한 장소를 검색해보세요 !"
       />
       <button
         type="submit"
-        className="flex items-center justify-center bg-[#8BE34A] px-4.5 lg:px-7"
+        className="flex flex-shrink-0 items-center justify-center rounded-r-2xl bg-[#8BE34A] px-4.5 lg:px-7"
       >
         <img src="/asset/searchbar/search.svg" alt="search"></img>
       </button>
