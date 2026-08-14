c4591c6bf854304d42e03360143e4c2a21797f2e
zldn109 <ss771009@naver.com>
2026-08-15 00:48:19 +0900

    Revert "feat: accessToken 만료 시 refreshToken으로 자동 재발급하는 인터셉터 추가"
    This reverts commit 7f942875995aa530cf4e3df1581ffde1c803eb85.

--- src/types/type.ts
+++ src/types/type.ts
@@ -138,15 +138,6 @@ export interface LoginResponse {
   data: LoginResponseProps;
 }
 
-export interface ReissueResponseProps {
-  accessToken: string;
-}
-
-export interface ReissueResponse {
-  message: string;
-  data: ReissueResponseProps;
-}
-
 export interface UserProfileResponseProps {
   name: string;
   nickname: string;
